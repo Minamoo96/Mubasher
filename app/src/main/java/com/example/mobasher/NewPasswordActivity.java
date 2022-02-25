9eced5f7233d1ae3afc99f46513e20d8b5f68da2
Minamoo96 <midoo3000@gmail.com>
2022-02-26 00:46:44 +0200

    last update 12:46 26/feb

--- app/src/main/java/com/example/mobasher/NewPasswordActivity.java
+++ app/src/main/java/com/example/mobasher/NewPasswordActivity.java
@@ -3,12 +3,34 @@ package com.example.mobasher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewPasswordActivity extends AppCompatActivity {
+
+    EditText repass1, repass2;
+    Button confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
+
+        repass1 = findViewById(R.id.new_repass);
+        repass2 = findViewById(R.id.new_repass2);
+        confirmPassword = findViewById(R.id.confirm_password);
+
+        confirmPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (repass1.getText().toString().equals(repass2.getText().toString())){
+                    Toast.makeText(NewPasswordActivity.this, "تم تأكيد كلمات المرور", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(NewPasswordActivity.this, "كلمات المرور ليست متطابقة. تأكد من أن كلمات المرور يجب ان تتطابق", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mobasher/OtpActivity.java
+++ app/src/main/java/com/example/mobasher/OtpActivity.java
@@ -3,15 +3,70 @@ package com.example.mobasher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class OtpActivity extends AppCompatActivity {
-
+    EditText code1, code2, code3, code4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        code1 = findViewById(R.id.input_code1);
+        code2 = findViewById(R.id.input_code2);
+        code3 = findViewById(R.id.input_code3);
+        code4 = findViewById(R.id.input_code4);
+
+        code1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (code1.getText().length()==1) {
+                    code2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        code2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (code2.getText().length() == 1) {
+                    code3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        code3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (code3.getText().length()==1) {
+                    code4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
 
         findViewById(R.id.resend).setOnClickListener(new View.OnClickListener() {
             @Override
