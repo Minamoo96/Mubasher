2597915b88e1425b85937a45bf30594a509ec8ba
Minamoo96 <midoo3000@gmail.com>
2022-02-25 21:12:52 +0200

    last update 9:11 25/feb

--- app/src/main/java/com/example/mobasher/OtpActivity.java
+++ app/src/main/java/com/example/mobasher/OtpActivity.java
@@ -3,6 +3,8 @@ package com.example.mobasher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class OtpActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class OtpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        findViewById(R.id.resend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(OtpActivity.this,"تم ارسال الرمز مرة اخرى" , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
