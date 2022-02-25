b84b1aa45a85226d46f43f27511378c44b359add
Minamoo96 <midoo3000@gmail.com>
2022-02-25 21:20:11 +0200

    last update 9:19 25/feb

--- app/src/main/java/com/example/mobasher/PasswordResetActivity.java
+++ app/src/main/java/com/example/mobasher/PasswordResetActivity.java
@@ -2,7 +2,9 @@ package com.example.mobasher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PasswordResetActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class PasswordResetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
+
+        findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PasswordResetActivity.this, NewPasswordActivity.class));
+            }
+        });
+
     }
 }
