d0d7db0b0a9fb8498d41551a23c2cd10a99a27b7
Minamoo96 <midoo3000@gmail.com>
2022-02-26 16:29:40 +0200

    User Experience last update 14:46 26/feb

--- app/src/main/java/com/example/mobasher/NewPasswordActivity.java
+++ app/src/main/java/com/example/mobasher/NewPasswordActivity.java
@@ -17,18 +17,22 @@ public class NewPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
-
-        repass1 = findViewById(R.id.new_repass);
-        repass2 = findViewById(R.id.new_repass2);
-        confirmPassword = findViewById(R.id.confirm_password);
-
+        {
+            repass1 = findViewById(R.id.new_repass);
+            repass2 = findViewById(R.id.new_repass2);
+            confirmPassword = findViewById(R.id.confirm_password);
+        }
         confirmPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (repass1.getText().toString().equals(repass2.getText().toString())){
-                    Toast.makeText(NewPasswordActivity.this, "تم تأكيد كلمات المرور", Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(NewPasswordActivity.this, "كلمات المرور ليست متطابقة. تأكد من أن كلمات المرور يجب ان تتطابق", Toast.LENGTH_LONG).show();
+                if (repass1.getText().toString().isEmpty() || repass2.getText().toString().isEmpty()) {
+                    Toast.makeText(NewPasswordActivity.this,"تاكد من ملئ كل الحقول.",Toast.LENGTH_LONG).show();
+                } else {
+                    if (repass1.getText().toString().equals(repass2.getText().toString())) {
+                        Toast.makeText(NewPasswordActivity.this, "تم تأكيد كلمات المرور", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(NewPasswordActivity.this, "كلمات المرور ليست متطابقة. تأكد من أن كلمات المرور يجب ان تتطابق", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/mobasher/RegisterActivity.java
+++ app/src/main/java/com/example/mobasher/RegisterActivity.java
@@ -5,18 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText fname, laname, phone, email, fpassword, lpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        {
+            fname = findViewById(R.id.text_field_first_name);
+            laname = findViewById(R.id.text_field_last_name);
+            phone = findViewById(R.id.text_field_phone);
+            email = findViewById(R.id.text_field_email);
+            fpassword = findViewById(R.id.text_field_password);
+            lpassword = findViewById(R.id.text_field_repass);
+        }
+
         findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this, OtpActivity.class));
+
+                if (fname.getText().toString().isEmpty() || fname.getText().toString().isEmpty() ||
+                phone.getText().toString().isEmpty() || email.getText().toString().isEmpty() ||
+                fpassword.getText().toString().isEmpty() || lpassword.getText().toString().isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "تأكد من ملئ كل الحقول.",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    if (fpassword.getText().toString().equals(lpassword.getText().toString())){
+                        Toast.makeText(RegisterActivity.this,"شكراً لتسجيلك معنا :" + fname + " "+laname,Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(RegisterActivity.this, OtpActivity.class));
+                    }else {
+                        Toast.makeText(RegisterActivity.this,"تأكد من كلمات المرور يجب يتشابها",Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         });
     }
