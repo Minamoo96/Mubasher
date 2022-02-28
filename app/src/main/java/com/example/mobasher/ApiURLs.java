08f394a62f37f3f829a6f09d7daa4f5b1609b034
Minamoo96 <midoo3000@gmail.com>
2022-03-01 00:44:54 +0200

    Adding API Services To Core Of Project last update 12:4 AM 01/Mar

---
+++ app/src/main/java/com/example/mobasher/ApiURLs.java
@@ -0,0 +1,10 @@
+package com.example.mobasher;
+
+public class ApiURLs {
+
+    private static final String Provider = "";
+    private static final String Provider1 = "";
+    private static final String Provider2 = "";
+    private static final String Provider3 = "";
+
+}
---
+++ app/src/main/java/com/example/mobasher/Services.java
@@ -0,0 +1,7 @@
+package com.example.mobasher;
+
+
+public class Services {
+
+
+}
